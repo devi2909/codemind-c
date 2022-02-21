1a1e8eb745c3cb045d29a58952692e540f29cdfa
devi2909 <98701535+devi2909@users.noreply.github.com>
2022-02-21 09:43:49 +0530

    #include<stdio.h> int main() {  int i,j,num;  scanf("%d",&num);  for(i=1;i<=num;i++)  {   for(j=1;j<=i;j++)   {    printf("%d",j);   }  printf(" ");  } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            if(j==i||j==1||j==n)
-            printf("* ");
-            else
-            printf("  ");
-        }
-        printf("
+ int i,j,num;
+ scanf("%d",&num);
+ for(i=1;i<=num;i++)
+ {
+  for(j=1;j<=i;j++)
+  {
+   printf("%d",j);
+  }
+ printf("
 ");
-    }
+ }
 }
