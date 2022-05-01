fce3e63df8f6f22c39e3a266381179bffe58e088
devi2909 <98701535+devi2909@users.noreply.github.com>
2022-05-01 12:38:26 +0530

    #include<stdio.h> #include<math.h> int main() {     double p,r,t,ci;     scanf("%lf%lf%lf",&p,&r,&t);     ci = p*pow((1+r/100),t);     printf("%.2lf",ci);     return 0; }

--- Compound_interest.c
+++ Compound_interest.c
@@ -2,11 +2,9 @@
 #include<math.h>
 int main()
 {
-    double p,r,t;
-    double ci;
+    double p,r,t,ci;
     scanf("%lf%lf%lf",&p,&r,&t);
-    scanf("%lf",&ci);
-    ci = p * pow((1+r/100),t);
+    ci = p*pow((1+r/100),t);
     printf("%.2lf",ci);
     return 0;
 }
